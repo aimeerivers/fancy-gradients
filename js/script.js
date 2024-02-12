09a4c62f22ad055c43de1c4e9919db90683f3afe
aimee rivers <aimee@aimeerivers.com>
2024-02-12 22:46:27 +0100

    feat: add the css for copy pasting

--- js/script.js
+++ js/script.js
@@ -9,8 +9,34 @@ boxes.forEach(box => {
   });
 });
 
+const cssOutput = document.getElementById('cssOutput');
+
+function updateCSSOutput() {
+  const cssOutput = document.getElementById('cssOutput');
+  const styles = window.getComputedStyle(demo);
+  let output = ".fancy {\n";
+
+  const backgroundProperties = ['background-image', 'background-color'];
+
+  for (let property of backgroundProperties) {
+    let value = styles.getPropertyValue(property);
+    if (!(property === 'background-color' && value === 'rgba(0, 0, 0, 0)')) {
+      output += `  ${property}: ${value};\n`;
+    }
+  }
+
+  output += "}";
+
+  output = output.replaceAll(/rgba\(0, 0, 0, 0\)/g, 'transparent');
+  output = output.replaceAll("radial-gradient", "\n    radial-gradient");
+
+  cssOutput.textContent = output;
+}
 
 function choose(className) {
   demo.removeAttribute('class');
   demo.classList.add(className);
+  updateCSSOutput();
 }
+
+updateCSSOutput();
